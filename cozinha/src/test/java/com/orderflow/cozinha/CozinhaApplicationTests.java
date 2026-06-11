c3bee82d834c41ceba286c37cdeb303ee0863f02
luan-filipin <luan.filipin@hotmail.com>
2026-06-11 16:38:15 -0300

    Primeiro commit

---
+++ cozinha/src/test/java/com/orderflow/cozinha/CozinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderflow.cozinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozinhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
